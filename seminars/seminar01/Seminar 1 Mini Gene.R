483c0019d104812665e831ee69b38ff5288bfcbb
Vivian Lac <vlac@uwo.ca>
2017-01-15 14:53:27 -0800

    Practicing on Mini Gene Expression Data Set from Seminar01

---
+++ seminars/seminar01/Seminar 1 Mini Gene.R
@@ -0,0 +1,65 @@
+# Read data from local mini gene expression dataset file
+prDat <- read.table("GSE4051_MINI.tsv.txt", header = TRUE, row.names = 1)
+
+# Upon successful import, str() the object as a sanity check
+str(prDat)
+
+# How many rows are there? Hint: nrow(), dim().
+nrow(prDat)
+dim(prDat)
+## answer: there are 36 rows!
+
+# How many columns or variables are there? Hint: ncol(), length(), dim()
+ncol(prDat)
+length(prDat)
+dim(prDat)
+## answer: there are 6 columns
+
+#Inspect the first few observations or the last few or a random sample. Hint: head(), tail(), x[i, j] combined with  sample().
+head(prDat)
+tail(prDat)
+
+# What does row correspond to – different genes or different mice?
+## answer: the row corresponds to different mice
+
+# What are the variable names? Hint: names(), dimnames().
+names(prDat)
+dimnames(prDat)
+## answer: sidNum, devStage, gType, crabHammer, eggBomb, poisonFang
+
+# What “flavor” is each variable, i.e. numeric, character, factor? Hint: str().
+str(prDat)
+## answer: sidNum = int / devStage = factor / gType = factor / crabHammer + eggBomb + poisonFang = numerical
+
+# For sample, do a sanity check that each integer between 1 and the number of rows in the dataset occurs exactly once. Hint: a:b, seq(), seq_len(), sort(), table(), ==, all(), all.equal(), identical().
+seq(prDat)
+seq_len(6)
+table(prDat)
+## I DON'T KNOW.... check github Stat540 R Markdown file for the answer!!
+
+# For each factor variable, what are the levels? Hint: levels(), str().
+attach(prDat)
+levels(devStage)
+levels(gType)
+str(prDat)
+## answer: devStage levels --> 4 weeks, E16, P10, P2, P6 // gType --> Nrlk0, WT
+
+# How many observations do we have for each level of devStage? For gType? Hint: summary(), table().
+summary(devStage)
+## answer: 4 weeks --> 8, E16 --> 7, P10 --> 8, P2 --> 8, P6 --> 8
+summary(gType)
+## answer: Nrlk0 --> 19, WT --> 20
+
+# Perform a cross-tabulation of devStage and gType. Hint: table().
+table(devStage, gType)
+
+# If you had to take a wild guess, what do you think the intended experimental design was? What actually happened in real life?
+## answer: to see if there was a difference in development stage between WT individuals and ones with NrlK0???
+## seems no difference was seen in real life??
+
+# For each quantitative variable, what are the extremes? How about average or median? Hint: min(), max(), range(),  summary(), fivenum(), mean(), median(), quantile().
+summary(sidNum)
+summary(crabHammer)
+summary(eggBomb)
+summary(poisonFang)
+## see output of above for the answers
